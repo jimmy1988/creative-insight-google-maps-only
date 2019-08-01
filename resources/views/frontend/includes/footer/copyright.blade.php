158aba84341211285f745cd3f4c1853f8750b5ce
James McHugh <james.mchugh1988@gmail.com>
2019-08-01 14:17:10 +0100

    moved old files out

---
+++ resources/views/frontend/includes/footer/copyright.blade.php
@@ -0,0 +1,6 @@
+<p>
+  <span>Creative Insight Developer Test.</span>
+  <span>@if (env('APP_AUTHOR')) {{ "Created By " .  env('APP_AUTHOR') . "."}} @endif</span>
+  <span>Copyright &copy; {{date("Y")}}.</span>
+  <span>All rights reserved.</span>
+</p>
