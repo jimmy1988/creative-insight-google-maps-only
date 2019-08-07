72a155d293710d5709bab7ac852d25145f1f771d
James McHugh <james.mchugh1988@gmail.com>
2019-08-07 10:16:52 +0100

    new bootstrap and font awesome

--- resources/views/frontend/includes/top/styles.blade.php
+++ resources/views/frontend/includes/top/styles.blade.php
@@ -1,9 +1,14 @@
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="/css/bootstrap.min.css">
+{{-- <link rel="stylesheet" href="/css/bootstrap.min.css"> --}}
+{{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"> --}}
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 <!-- Simple line Icon -->
 <link rel="stylesheet" href="/css/simple-line-icons.css">
 <!-- Themify Icon -->
 <link rel="stylesheet" href="/css/themify-icons.css">
+<!--Font Awesome-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <!-- Hover Effects -->
 <link rel="stylesheet" href="/css/set1.css">
 <!-- Main CSS -->
