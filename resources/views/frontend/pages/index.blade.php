6d9c22a3e5057c5520e6503567b6c61524a04e70
James McHugh <james.mchugh1988@gmail.com>
2019-10-03 16:33:08 +0100

    structure modification

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -2,34 +2,43 @@
 
 @section('content')
 
-<div class="container">
+<div class="container-fluid no-gutters" id="map-container">
   <form method="post" action="" id="searchForm">
     <div class="row" id="search-container">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-1 col-xl-1">
-        &nbsp;
-      </div>
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-10 col-xl-10">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
           <div class="form-group">
             <div class="row">
-              <div id="search-box-container" class="col-md-10 col-lg-10 col-xl-10">
-                <input type="text" id="search-box" class="form-control" placeholder="Search e.g.Nandos or Restaurants Near Me"/>
+              <div id="search-box-container" class="col-md-8">
+                <div class="form-group">
+                  <input type="text" id="search-box" class="form-control" placeholder="Search For Businesses e.g.Nandos"/>
+                </div>
               </div>
-              <div id="search-button-container" class="col-md-2 col-lg-2 col-xl-2">
-                <button type="submit" id="search-button" class="btn btn-secondary">Search</button>
+              <div id="distance-container" class="col-md-2">
+                <div class="form-group">
+                  <select class="form-control" id="distance-box">
+                    @for ($i=0; $i <= 25; $i)
+                      <option value="{{$i}}">{{$i}} Miles +</option>
+                      <?php $i = $i + 5; ?>
+                    @endfor
+                  </select>
+                </div>
+              </div>
+              <div id="search-button-container" class="col-md-2">
+                <button type="submit" id="search-button" class="btn btn-secondary"><i class="fas fa-crosshairs"></i></button>
               </div>
             </div>
           </div>
       </div>
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-1 col-xl-1">
-        &nbsp;
-      </div>
     </div>
   </form>
   <div class="row" id="google-map-container">
-    <div class="col-12">
+    <div class="col-md-9">
       <div id="map">
-        
+
       </div>
+    </div>
+    <div class="col-md-3">
+
     </div>
   </div>
 </div>
