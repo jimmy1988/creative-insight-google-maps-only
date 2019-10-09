e155891fd819ca2cef61ad591c92ba339a832089
James McHugh <james.mchugh1988@gmail.com>
2019-10-09 12:31:08 +0100

    completed map with directions

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -3,95 +3,99 @@ var defaultLong = -1.9001709;
 var zoomlevel = 16;
 var map;
 var infoWindow;
+var marker;
 var center;
 var markers = [];
 var currentloc = {};
 var currentlocDetails = {};
 var request;
 var service;
+var distanceMatrix;
+var places = [];
+var distanceMiles;
+var distanceMeters;
 
 
-function initializeMap() {
-  $("#curtain-outer").show();
+function checkAdult(age) {
+  return age >= 18;
+}
 
-  center = {lat: defaultLat, lng: defaultLong};
-
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: center,
-    zoom: zoomlevel
-  });
-
-  infoWindow = new google.maps.InfoWindow;
-
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      currentloc.lat = parseFloat(position.coords.latitude);
-      currentloc.lng = parseFloat(position.coords.longitude);
-
-      var otherOptions = {};
-      otherOptions.icon =  {
-         url: "https://image.flaticon.com/icons/svg/60/60834.svg", // url
-         scaledSize: new google.maps.Size(30, 30)
-       };
-
-      otherOptions.name = "Your Location";
-      otherOptions.context = "Home";
-      otherOptions.animation = google.maps.Animation.BOUNCE;
-
-      createmarker(currentloc, otherOptions);
-
-      map.setCenter(currentloc);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
-    }, {maximumAge:100, timeout:5000, enableHighAccuracy: true});
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
-
-  $("#curtain-outer").fadeOut(500);
+function myFunction() {
+  document.getElementById("demo").innerHTML = ages.findIndex(checkAdult);
 }
 
 //Removes the markers from the map, but keeps them in the array.
-function clearAllMarkers() {
+function clearAllMarkers(removeFromMap = true, removeFromArray = false, removeFromMapExceptions = [], removeFromArrayExceptions = []) {
   if(markers.length > 1){
-    for (var i = 1; i < markers.length; i++) {
+    for (var i = 0; i < markers.length; i++) {
+      if(removeFromMap == true){
+        if(removeFromMapExceptions.indexOf(i) == -1){
           markers[i].setMap(null);
+        }
+      }
+
+      if(removeFromArray == true){
+        if(removeFromArrayExceptions.indexOf(i) == -1){
           markers.splice(i, 1);
+          places.splice(i, 1);
+        }
+      }
     }
   }
 }
 
-function createmarker(place, otherMapOptions = null){
+function createListItem(place = null, distance = 0, markersArrayIndex = 0, placesArrayIndex = 0){
+  var html = "";
+  if(place != null && place != undefined && place != ""){
 
-  var mapOptions = {};
-  mapOptions.map = map;
-  if(
-    place.geometry != undefined && place.geometry != null &&
-    place.geometry.location != undefined && place.geometry.location != null
-  ){
-    mapOptions.position = place.geometry.location;
-  }else{
-    mapOptions.position = {lat: parseFloat(place.lat), lng: parseFloat(place.lng)};
+    html = html + "<div class=\"map-entry\">";
+    html = html + "<div class=\"row\">";
+    html = html + "<div class=\"col-md-12\">";
+    html = html + "<h6 class=\"map-entry-title text-center\">" + place.name + "</h6>";
+    html = html + "<p class=\"map-entry-address\">" + place.vicinity + "</p>";
+    html = html + "<p class=\"map-entry-miles\"><span>" + distance + "</span><span> Miles Away</span></p>";
+    html = html + "</div>";
+    html = html + "</div>";
+    html = html + "<div class=\"row\">";
+    html = html + "<div class=\"col-md-12\">";
+    html = html + "<p class=\"rating\">Rating:&nbsp;";
+    html = html + "<span class=\"rating-stars\">";
+
+    var rating = parseFloat(place.rating);
+    var ratingFloor = Math.floor(place.rating);
+    var roundedRating = Math.round(place.rating * 2)/2;
+    roundedRating = parseFloat(roundedRating.toFixed(1));
+
+    if(rating > 0.5 && rating < 1){
+      html = html + "<i class=\"fas fa-star-half-alt\"></i>";
+    }else if(rating >= 1){
+      for(var i = 0; i < ratingFloor; i++){
+        html = html + "<i class=\"fas fa-star\"></i>";
+      }
+
+      var halfRating = parseFloat(rating) - parseFloat(i);
+      if(halfRating >= 0.5){
+        html = html + "<i class=\"fas fa-star-half-alt\"></i>";
+      }
+    }
+
+
+    html = html + "</span>";
+    html = html + "&nbsp;";
+    html = html + "<span>" + place.rating + "</span>";
+    html = html + "</p>";
+    html = html + "</div>";
+    html = html + "<div class=\"col-md-12 text-right\">";
+    html = html + "<a href=\"#\" onclick=\"getDirections(event, this," + markersArrayIndex + ", " + placesArrayIndex + ")\" class=\"btn btn-info get-directions-button\">Get Directions</a>";
+    html = html + "</div>";
+    html = html + "</div>";
+    html = html + "</div>";
   }
 
+  return html;
+}
 
-  if(otherMapOptions != null){
-    mapOptions = Object.assign(mapOptions, otherMapOptions);
-  }
-
-  var marker = new google.maps.Marker(mapOptions);
-  if(
-    (otherMapOptions != undefined && otherMapOptions != null) &&
-    (otherMapOptions.context != undefined && otherMapOptions.context != null && otherMapOptions.context == "Home")
-  ){
-    currentlocDetails.marker = marker;
-  }else{
-    markers.push(marker);
-  }
-  map.setCenter(currentloc);
-
+function createInfoWindow(place = null, infowindowOptions = null){
   if(place != undefined && place != null && place.name != undefined && place.name != null){
     var infowindow = new google.maps.InfoWindow({
       content: place.name
@@ -99,15 +103,65 @@ function createmarker(place, otherMapOptions = null){
     marker.addListener('click', function(){
       infowindow.open(map, this);
     });
-  }else if (otherMapOptions != undefined && otherMapOptions != null && otherMapOptions.name != undefined && otherMapOptions.name != null) {
+  }else if (infowindowOptions != undefined && infowindowOptions != null && infowindowOptions.name != undefined && infowindowOptions.name != null) {
     var infowindow = new google.maps.InfoWindow({
-      content: otherMapOptions.name
+      content: infowindowOptions.name
     });
     marker.addListener('click', function(){
       infowindow.open(map, this);
     });
   }
-  map.setCenter(currentloc);
+}
+
+function createmarker(place = null, markerOptions = null){
+
+  var marker = new google.maps.Marker;
+  marker.setMap(map);
+
+  if(
+    place.geometry != undefined && place.geometry != null &&
+    place.geometry.location != undefined && place.geometry.location != null
+  ){
+    marker.setPosition(place.geometry.location);
+  }else{
+    marker.setPosition({lat: parseFloat(place.lat), lng: parseFloat(place.lng)});
+  }
+
+  return marker;
+}
+
+function getDirections(event, elem, markersArrayIndex = -1, placesArrayIndex = -1){
+  event.preventDefault();
+  if(placesArrayIndex != undefined && placesArrayIndex != null && placesArrayIndex >= 0){
+    clearAllMarkers(true, false);
+
+    var directionsService = new google.maps.DirectionsService();
+    var directionsRenderer = new google.maps.DirectionsRenderer();
+    var thisPlace = places[placesArrayIndex];
+
+    // var home = new google.maps.LatLng(center);
+    var home = center;
+    var destination = thisPlace.geometry.location;
+
+    directionsRenderer.setMap(map);
+
+    var request = {
+      origin: home,
+      destination: destination,
+      travelMode: 'DRIVING'
+    };
+
+    directionsService.route(request, function(result, status) {
+      if (status == 'OK') {
+        directionsRenderer.setDirections(result);
+        console.log(result);
+      }
+    });
+
+
+  }else{
+    alert("No location found");
+  }
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -121,22 +175,94 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 function plotMarkers(results, status){
   if(status == google.maps.places.PlacesServiceStatus.OK){
     for(var i = 0; i < results.length; i++){
-      createmarker(results[i]);
+
+      var distance = google.maps.geometry.spherical.computeDistanceBetween(new google.maps.LatLng(currentloc.lat, currentloc.lng), results[i].geometry.location);
+      distance = parseFloat(distance) / parseFloat("1609.344");
+      distance = parseFloat(distance.toFixed(1));
+
+      var totalDistance = parseInt(distanceMiles) + 5;
+
+      if(distance <= totalDistance){
+        places.push(results[i]);
+        var thisMarker = createmarker(results[i]);
+        markers.push(thisMarker);
+        var thisInfoWindow = createInfoWindow(results[i], thisMarker);
+        $("#rightPaneContent").append(createListItem(results[i], distance, i, i));
+      }else{
+        break;
+      }
     }
+
+    zoomlevel = 10;
+    map.setZoom(zoomlevel);
   }
+}
+
+function initializeMap() {
+  $("#curtain-outer").show();
+
+  center = new google.maps.LatLng(defaultLat, defaultLong);
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: center,
+    zoom: zoomlevel
+  });
+
+  infoWindow = new google.maps.InfoWindow;
+  marker = new google.maps.Marker;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      currentloc.lat = parseFloat(position.coords.latitude.toFixed(7));
+      currentloc.lng = parseFloat(position.coords.longitude.toFixed(7));
+
+      center = new google.maps.LatLng(currentloc.lat, currentloc.lng);
+
+      var icon = {
+        url: "https://image.flaticon.com/icons/svg/60/60834.svg",
+        scaledSize: new google.maps.Size(30, 30)
+      };
+
+      marker.setPosition(center);
+      marker.setMap(map);
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      marker.setIcon(icon);
+      marker.setVisible(true);
+
+      var infowindow = new google.maps.InfoWindow({
+        content: "Your Location"
+      });
+      marker.addListener('click', function(){
+        infowindow.open(map, this);
+      });
+
+      map.setCenter(currentloc);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    }, {maximumAge:50, timeout:5000, enableHighAccuracy: true});
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+  $("#curtain-outer").fadeOut(500);
 }
 
 function searchMap(){
   event.preventDefault();
   $("#curtain-outer").fadeIn(500, function(){
-    clearAllMarkers();
+    clearAllMarkers(true, true);
+    $("#rightPaneContent").html("");
+    zoomlevel = 16;
+    map.setZoom(zoomlevel);
     var searchTerm = document.getElementById("search-box").value;
-    var distanceMiles = document.getElementById("distance-box").value;
-    var distanceMeters = Math.ceil((parseFloat(distanceMiles) * parseFloat("1609.344")) + parseFloat(distanceMiles));
+    distanceMiles = document.getElementById("distance-box").value;
+    distanceMeters = Math.ceil((parseFloat(distanceMiles) + parseFloat(5)) * parseFloat("1609.344"));
 
     request = {
       location:center,
-      radius:distanceMeters,
+      rankBy:google.maps.places.RankBy.DISTANCE,
       name: [searchTerm],
       types:[searchTerm]
     };
